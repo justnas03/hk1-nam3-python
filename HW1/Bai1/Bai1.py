2f5f9e0a2d6c2a5039f519ed6acf4e54a945bb0b
Thuan Do Laptop <thuando.contact@gmail.com>
2024-09-04 18:46:25 +0700

    test bai4

---
+++ HW1/Bai1/Bai1.py
@@ -0,0 +1,32 @@
+def bai1():
+    print("16*4 =",16*4)
+    print("16**4 =",16**4)
+    print("6/9 =",6/9)
+    print("16//24 =",16//24)
+    print("4/0 = khong duoc do chia cho 0")
+    
+def bai2():
+    global ten 
+    ten =input("Nhap ten: ")
+    diachi = input("Nhap dia chi: ")
+    namsinh = int(input("Nhap nam sinh: "))
+    print("Thong tin ca nhan: ")
+    print("+ Ten: ",ten)
+    print("+ Dia chi: ",diachi)
+    print("+ Nam sinh: ",namsinh)
+
+def bai3():
+    ten = "Dixon"
+    print("Your name is: ", ten)
+    ten = "Daryl"
+    print("Your name is: ", ten)
+
+def bai4():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    # bai1()
+    # bai2()
+    # bai3()
+    bai4()
