e82a49be3ea59a49528036302fa6fc6979dc8971
Thuan Do PC <thuando.contact@gmail.com>
2024-09-05 10:54:30 +0700

    Done bai tap chuong 1 String

---
+++ HW1/String.py
@@ -0,0 +1,28 @@
+def bai1():
+    S = input("Enter the String: ")
+    count = 0
+    for i in S:
+        count += 1
+    print("Lenght of the string is:", count)
+
+def bai2():
+    S = input("Enter the String: ")
+    count = 0
+    for i in S:
+        if i == " ":
+            count += 1
+    print("Number of words in the string is:", count+1)
+
+def bai3():
+    S = input("Enter the String: ")
+    count = 0
+    vowels = "aeiou"
+    for i in S:
+        if i in vowels:
+            count += 1
+    print("Number of vowels in the string is:", count)
+
+if __name__ == "__main__":
+    # bai1()
+    # bai2()
+    bai3()
