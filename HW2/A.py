f28fc4203d63fe52a829e5270e94b03fa4a07993
Thuan Do PC <thuando.contact@gmail.com>
2024-09-05 13:14:58 +0700

    xong cau A

---
+++ HW2/A.py
@@ -0,0 +1,19 @@
+def bai1():
+    x = 8
+    y = 2
+    print(x + y * 3)
+    print((x + y) * 3)
+    print(x ** y)
+    print(x % y)
+    print(x / 12.0)
+    print(x // 6)
+
+def bai2():
+    x = 54.66
+    print(round(x))
+    print(int(x))
+
+if __name__ == "__main__":
+    # bai1()
+    bai2()
+
